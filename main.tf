ada86e8702582f1df8269fa9e946c33d93d82402
hareeshb808 <hareeshb808@gmail.com>
2025-03-24 15:25:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ terraform {
       version = ">=4.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "NextOps"  
+    storage_account_name = "nextopstfsat20"                      
+    container_name       = "tfstate"                       
+    key                  = "terraform.tfstate"
+    access_key          = "jfaksdnkcnidjcijdiankdnkndinikniknikanfidif"        
+  }
+}
 }
 
 provider "azurerm" {
