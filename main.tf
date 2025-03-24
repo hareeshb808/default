2df2b2685b2a6502b6a4347b38510ab7d54ba2e6
hareeshb808 <hareeshb808@gmail.com>
2025-03-24 14:44:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
 
